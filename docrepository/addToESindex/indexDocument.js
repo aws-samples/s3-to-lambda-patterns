675997cec397748d6f4250e0baff89e5c7dea7d7
Monika Mevenkamp <monikam@princeton.edu>
2020-06-12 16:56:40 -0400

    fix host in request header

--- docrepository/addToESindex/indexDocument.js
+++ docrepository/addToESindex/indexDocument.js
@@ -15,7 +15,7 @@
 */
 
 const AWS = require('aws-sdk')
-AWS.config.region = process.env.AWS_REGION 
+AWS.config.region = process.env.AWS_REGION
 const type = '_doc'
 
 const indexDocument = async (event) => {
@@ -23,18 +23,18 @@ const indexDocument = async (event) => {
     const endpoint = new AWS.Endpoint(process.env.domain)
     let request = new AWS.HttpRequest(endpoint, process.env.AWS_REGION)
     const document = event.content
-  
+
     request.method = 'PUT'
     request.path += event.index + '/' + type + '/' + event.id
     request.body = JSON.stringify(document)
-    request.headers['host'] = process.env.domain
+    request.headers['host'] = endpoint.host
     request.headers['Content-Type'] = 'application/json';
     request.headers['Content-Length'] = Buffer.byteLength(request.body)
-  
+
     const credentials = new AWS.EnvironmentCredentials('AWS')
     const signer = new AWS.Signers.V4(request, 'es')
     signer.addAuthorization(credentials, new Date())
-  
+
     const client = new AWS.HttpClient()
     client.handleRequest(request, null, function(response) {
       console.log(response.statusCode + ' ' + response.statusMessage)
