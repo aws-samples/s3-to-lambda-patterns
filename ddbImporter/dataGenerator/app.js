97252a12897b26a15d5d998d6429d36a903eb188
James Beswick <jbeswick@amazon.com>
2021-08-06 12:01:18 -0400

    Added DATA_DIR constant

--- ddbImporter/dataGenerator/app.js
+++ ddbImporter/dataGenerator/app.js
@@ -1,17 +1,6 @@
-/*
-  Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this
-  software and associated documentation files (the "Software"), to deal in the Software
-  without restriction, including without limitation the rights to use, copy, modify,
-  merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so.
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/*! Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *  SPDX-License-Identifier: MIT-0
+ */
 
 const faker = require('faker')
 const fs = require('fs')
@@ -21,6 +10,7 @@ const fs = require('fs')
 
 const MAX_RECORDS = parseInt(process.argv[2])
 const MAX_FILES = parseInt(process.argv[3])
+const DATA_DIR = './data'
 
 if (!MAX_RECORDS || !MAX_FILES)
   return console.error('Missing command line arguments.')
@@ -51,5 +41,5 @@ for (let j = 1; j <= MAX_FILES; j++) {
       lng: faker.address.longitude() 
     })
   }
-  fs.writeFileSync(`./data/import-${j}.json`, JSON.stringify(records))
+  fs.writeFileSync(`./${DATA_DIR}/import-${j}.json`, JSON.stringify(records))
 }
